75d3af5c321ac1d4bf8e65055332463b2a8cfa1c
Kevin Nguyen <Kevin.nguyen801@gmail.com>
2018-05-11 16:03:42 -0600

    Asks for permission on startup instead of on pressing Navigation View
    -Reorganized code

--- app/src/main/java/kevin801/deliveryassistant/maps/MapsActivity.java
+++ app/src/main/java/kevin801/deliveryassistant/maps/MapsActivity.java
@@ -1,34 +1,21 @@
 package kevin801.deliveryassistant.maps;
 
 import android.Manifest;
-import android.app.Activity;
-import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
-import android.widget.Toast;
-
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GooglePlayServicesUtil;
-import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.LocationSource;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 
@@ -37,7 +24,6 @@ import kevin801.deliveryassistant.R;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
     
     private GoogleMap mMap;
-    private int LOCATION_PERMISSION_REQUEST = 1;
     private FusedLocationProviderClient mFusedLocationProviderClient;
     
     @Override
